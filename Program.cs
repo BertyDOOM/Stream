2c76c6099b914e6c345cefbe17c277085ec3239a
Bertan <Bertan@DESKTOP-7AG4V62>
2024-11-27 09:59:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+string dir ="Some.txt";
+string newdir = "Some-2.txt";
+//string dir ="Some-2.txt";
+//string newdir ="Some.txt";
+    bool exists = false;
+
+if (Path.Exists(dir))
+{
+    StreamReader streamReader = new StreamReader(dir);
+    using (streamReader)
+    {
+        string line;
+        while ((line = streamReader.ReadLine()) != null)
+        {
+            Console.WriteLine("File content: "+line);
+            streamReader.ReadLine();
+        }
+    }
+    exists = true;
+    Console.WriteLine("File exists: "+exists);
+    try
+    {
+        File.Move(dir, newdir);
+        Console.WriteLine("Renamed to " + newdir);
+    }
+    catch
+    {
+        Console.WriteLine("Error");
+    }
+}
+else 
+{
+    Console.WriteLine("File exists: "+exists);
+}
